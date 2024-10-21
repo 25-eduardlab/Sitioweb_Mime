30b9fb997ff2aa2b8b8a5c9b9e27e39e84910f47
Diego <diegopalomino739@gmail.com>
2024-10-21 11:48:59 -0500

    Refactorizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import Content from './components/Content';
 import PageFooter from './components/PageFooter'
 import './App.css';
+import Conectate from './components/Conectate';
+import Colaboraciones from './components/Colaboraciones';
+import Acercamime from './components/Acercamime';
+import Ofrecemos from './components/Ofrecemos';
+
 
 const App = () => {
 
@@ -11,23 +15,14 @@ const App = () => {
   return (
     <>
       <Header />
-      <Content />
+      <div className="content-container">
+      <Conectate/>
+      <Colaboraciones/>
+      <Acercamime/>
+      <Ofrecemos/>
+      </div>   
+      <PageFooter/>
       
-      {/* Aquí puedes agregar más componentes */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       
     </>
   );
---
+++ src/components/Conectate.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import '../styles/conectate.css'
+
+
+
+const Conectate =()=>{
+    return (
+         
+        
+          <section className="video-section">
+            <div className="bienvenida">
+              <h1>Impulsa tu Vision</h1>
+              <h2>Conectate al futuro con exito</h2>
+              <h3>Empieza tu futuro hoy</h3>
+              <div className="fondo"></div>
+            </div>
+          </section>
+          
+
+    );
+};
+
+export default Conectate;
