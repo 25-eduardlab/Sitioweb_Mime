46128f2f3c181bbcf14c2fb939422c57b00cf209
James <toji6615@gmail.com>
2024-11-15 10:33:08 -1200

    Finish Slider

--- src/components/Nuestrosproyectos.jsx
+++ src/components/Nuestrosproyectos.jsx
@@ -6,13 +6,11 @@ import IA4 from '../img/IA4.jpeg';
 import IA1 from '../img/IA1.jpg';
 
 const NuestrosProyectos = () => {
-  // Estado para el índice del slider de la primera imagen
+  
   const [currentIndex, setCurrentIndex] = useState(0);
   
-  // Aquí es donde puedes agregar más imágenes al slider
-  const sliderImages = [HolaIA, IA1, IA2, IA4]; // Imágenes para el slider
+  const sliderImages = [HolaIA, IA1, IA2, IA4]; 
   
-  // Función para cambiar la imagen del slider
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % sliderImages.length);
   };
@@ -21,7 +19,6 @@ const NuestrosProyectos = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + sliderImages.length) % sliderImages.length);
   };
 
-  // Función para manejar las teclas del teclado (flechas)
   const handleKeyPress = (event) => {
     if (event.key === 'ArrowRight') {
       nextSlide();
@@ -30,17 +27,15 @@ const NuestrosProyectos = () => {
     }
   };
 
-  // Cambiar la imagen del slider cada 3 segundos automáticamente
   useEffect(() => {
     const interval = setInterval(nextSlide, 3000);
-    return () => clearInterval(interval); // Limpiar el intervalo cuando el componente se desmonte
+    return () => clearInterval(interval); 
   }, []);
 
-  // Añadir el escuchador de eventos para las teclas del teclado
+  
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress);
 
-    // Limpiar el escuchador cuando el componente se desmonte
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
@@ -52,16 +47,13 @@ const NuestrosProyectos = () => {
         <span className="highlight">Nuestros</span> proyectos
       </h2>
       <div className="proyectos-grid">
-        {/* Contenedor principal con Grid */}
         <div className="left-side">
-          {/* Slider solo para la primera imagen */}
           <div className="slider-container">
             <div className="slider">
             
               <img src={sliderImages[currentIndex]} alt={`Proyecto ${currentIndex + 1}`} />
             </div>
 
-            {/* Controles de navegación ocultos */}
             <button className="prev" onClick={prevSlide}>
               &#10094;
             </button>
@@ -71,7 +63,6 @@ const NuestrosProyectos = () => {
           </div>
         </div>
 
-        {/* Lado derecho con imágenes estáticas */}
         <div className="right-side">
           <div className="proyecto-item">
             <img src={IA2} alt="Proyecto 2" />
